aefd03d5157db501f30365af253e5b589abf6d46
Precious-c <preciousamaechi111@yahoo.com>
2022-11-06 01:52:55 -0700

    list all records in order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- Lists all record of the table second_table of database hbtn_0c_0. 
+-- Displays only scores and names ordered by scores in descending order
+
+SELECT score, name
+FROM second_table 
+ORDER BY score DESC
